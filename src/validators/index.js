71745c178a3a6b50e9adb87b3bf56f40b6962e71
Derek Miranda <derek.miranda.1992@gmail.com>
2022-11-17 12:34:51 -0500

    creates email + pw validators

---
+++ src/validators/index.js
@@ -0,0 +1,27 @@
+// validators return string for error message if invalid
+// otherwise, they return an empty string
+
+// borrowed from: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s01.html
+const EMAIL_REGEX =
+  /^[\w!#$%&'*+/=?`{|}~^-]+(?:\.[\w!#$%&'*+/=?`{|}~^-]+)*@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$/i;
+const MAX_EMAIL_CHARS = 50;
+const MIN_PASSWORD_CHARS = 4;
+const MAX_PASSWORD_CHARS = 16;
+
+export const validateEmail = (password) => {
+  if (!EMAIL_REGEX.test(password)) {
+    return "Not a valid password";
+  } else if (password.length > MAX_EMAIL_CHARS) {
+    return `Must be under ${MAX_EMAIL_CHARS} characters`;
+  }
+  return "";
+};
+
+export const validatePassword = (password) => {
+  if (password.length > MIN_PASSWORD_CHARS) {
+    return `Must be over ${MIN_PASSWORD_CHARS} characters`;
+  } else if (password.length > MAX_PASSWORD_CHARS) {
+    return `Must be under ${MAX_PASSWORD_CHARS} characters`;
+  }
+  return "";
+};
