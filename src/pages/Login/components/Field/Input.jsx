5354f66abb28561d23f343fcd63d241222fb694d
Derek Miranda <derek.miranda.1992@gmail.com>
2022-11-17 10:40:17 -0500

    FieldInput accounts for icons

--- src/pages/Login/components/Field/Input.jsx
+++ src/pages/Login/components/Field/Input.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 
 import styles from "./Input.module.scss";
 
-const FieldInput = ({ children, ...props }) => {
-  return <input type="text" className={styles.input} {...props} />;
+const FieldInput = ({ children, icon: Icon, ...props }) => {
+  return (
+    <div className={styles.wrapper}>
+      {Icon && <Icon className={styles.icon} />}
+      <input
+        type="text"
+        className={classNames(styles.input, {
+          [styles.input__hasIcon]: !!Icon,
+        })}
+        {...props}
+      />
+    </div>
+  );
 };
 
 export default FieldInput;
