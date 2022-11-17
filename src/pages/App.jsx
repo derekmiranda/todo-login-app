36736f9ebf334b05c39f37becdc7ed3242b58af2
Derek Miranda <derek.miranda.1992@gmail.com>
2022-11-17 17:42:49 -0500

    Starts TodoPage

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import LoginPage from "./Login";
+import TodosPage from "./Todos";
 
 import useAuth from "/src/hooks/useAuth";
 
 const App = () => {
-  const { loggedIn, login } = useAuth();
-  return loggedIn ? <main>todos</main> : <LoginPage login={login} />;
+  const { loggedIn, login, logout } = useAuth();
+  return loggedIn ? <TodosPage logout={logout} /> : <LoginPage login={login} />;
 };
 
 export default App;
---
+++ src/pages/Todos/index.jsx
@@ -0,0 +1,15 @@
+import React, { useCallback, useEffect } from "react";
+
+import Page from "/src/components/Page";
+
+import styles from "./index.module.scss";
+
+const TodosPage = ({ logout }) => {
+  return (
+    <Page>
+      <h1>Todos</h1>
+    </Page>
+  );
+};
+
+export default TodosPage;
