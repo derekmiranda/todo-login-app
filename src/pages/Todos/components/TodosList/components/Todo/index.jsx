fd5dd84e89f3779cc64aee855e0bd4c75e56d29f
Derek Miranda <derek.miranda.1992@gmail.com>
2022-11-18 17:57:28 -0500

    handles removing todos

--- src/pages/Todos/components/TodosList/components/Todo/index.jsx
+++ src/pages/Todos/components/TodosList/components/Todo/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import styles from "./index.module.scss";
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, onRemove }) => {
   return (
     <li className={styles.todo}>
       <input type="text" value={todo.task} className={styles.todo_input} />
       <button>Edit</button>
-      <button>Remove</button>
+      <button data-remove-id={todo.id} onClick={onRemove}>
+        Remove
+      </button>
     </li>
   );
 };
--- src/pages/Todos/components/TodosList/index.jsx
+++ src/pages/Todos/components/TodosList/index.jsx
@@ -4,8 +4,17 @@ import Todo from "./components/Todo";
 
 import styles from "./index.module.scss";
 
-const TodosList = ({ todos = [], addTodo }) => {
-  const todoEls = todos.map((todo) => <Todo key={todo.id} todo={todo} />);
+const TodosList = ({ todos = [], removeTodo }) => {
+  const onRemove = useCallback((event) => {
+    const removeId = event.target?.dataset.removeId;
+    if (removeId) {
+      removeTodo(removeId);
+    }
+  });
+
+  const todoEls = todos.map((todo) => (
+    <Todo key={todo.id} todo={todo} onRemove={onRemove} />
+  ));
 
   return <ol className={styles.list}>{todoEls}</ol>;
 };
