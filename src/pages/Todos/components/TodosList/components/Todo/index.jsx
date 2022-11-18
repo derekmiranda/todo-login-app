d40b975ed9b9f673ca95c240d7a990a5763a5dae
Derek Miranda <derek.miranda.1992@gmail.com>
2022-11-18 18:07:47 -0500

    edit state for todo

--- src/pages/Todos/components/TodosList/components/Todo/index.jsx
+++ src/pages/Todos/components/TodosList/components/Todo/index.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 
 import styles from "./index.module.scss";
 
-const Todo = ({ todo, onRemove }) => {
+const Todo = ({ todo, onRemove, onUpdate }) => {
+  const inputRef = useRef(null);
+
+  const focusInput = useCallback(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [inputRef.current]);
+
   return (
     <li className={styles.todo}>
-      <input type="text" value={todo.task} className={styles.todo_input} />
-      <button>Edit</button>
+      <input
+        type="text"
+        ref={inputRef}
+        value={todo.task}
+        className={styles.todo_input}
+        onChange={onUpdate}
+        data-update-id={todo.id}
+      />
+      <button onClick={focusInput}>Edit</button>
       <button data-remove-id={todo.id} onClick={onRemove}>
         Remove
       </button>
