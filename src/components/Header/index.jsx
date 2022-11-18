9c03b2aa447e437f1d394b1541e61186f30cfa0b
Derek Miranda <derek.miranda.1992@gmail.com>
2022-11-18 11:50:31 -0500

    removes Header - unnecessary

--- src/components/Header/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./index.module.scss";
-
-const Header = ({ children }) => {
-  return <div className={styles.header}>{children}</div>;
-};
-
-export default Header;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,7 +4,6 @@ import FieldWrapper from "./components/Field/Wrapper";
 import FieldLabel from "./components/Field/Label";
 import FieldInput from "./components/Field/Input";
 import Page from "/src/components/Page";
-import Header from "/src/components/Header";
 import LoginButton from "./components/LoginButton";
 
 import styles from "./index.module.scss";
@@ -50,7 +49,6 @@ const LoginPage = ({ login }) => {
 
   return (
     <Page>
-      <Header />
       <h1>Rapptr Labs</h1>
       <form
         id={FORM_ID}
