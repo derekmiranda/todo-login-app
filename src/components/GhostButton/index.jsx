f63956f9aad8fb22713262ab12abe29dd145af8b
Derek Miranda <derek.miranda.1992@gmail.com>
2022-11-19 08:03:10 -0500

    adds GhostButton

---
+++ src/components/GhostButton/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classNames from "classnames";
+
+import styles from "./index.module.scss";
+
+const GhostButton = ({ children, className, ...props }) => {
+  return (
+    <button className={classNames(styles.ghost)} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default GhostButton;
