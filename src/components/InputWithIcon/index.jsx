20bdfdff295cd1c09ab4494d5450f66549eb91f0
Derek Miranda <derek.miranda.1992@gmail.com>
2022-11-19 07:57:18 -0500

    inputwithicon has extendable classname

--- src/components/InputWithIcon/index.jsx
+++ src/components/InputWithIcon/index.jsx
@@ -3,15 +3,19 @@ import classNames from "classnames";
 
 import styles from "./index.module.scss";
 
-const InputWithIcon = ({ children, icon: Icon, ...props }) => {
+const InputWithIcon = ({ children, className, icon: Icon, ...props }) => {
   return (
     <div className={styles.wrapper}>
       {Icon && <Icon className={styles.icon} />}
       <input
         type="text"
-        className={classNames(styles.input, {
-          [styles.input__hasIcon]: !!Icon,
-        })}
+        className={classNames(
+          styles.input,
+          {
+            [styles.input__hasIcon]: !!Icon,
+          },
+          className
+        )}
         {...props}
       />
     </div>
